36290c5ba831add1480188631a99a509c42a906c
Joshy Cyriac <j.cyriac@gmail.com>
2018-05-09 17:46:45 +0200

    somehow output and mask sizes don't match

--- AortaDataset.py
+++ AortaDataset.py
@@ -2,9 +2,10 @@ import glob
 
 import nibabel as nib
 import torch
+import torchvision
 from skimage import transform
 from torch.utils.data import Dataset
-
+import numpy as np
 
 class AortaDataset(Dataset):
     """ Aorta Dataset helper class for pytorch. """
@@ -22,10 +23,16 @@ class AortaDataset(Dataset):
         mask, image = self.pairs[idx]
         return self.__get_zero_slice(image), self.__get_zero_slice(mask)
 
+    def _normalize(self, v):
+        norm = np.linalg.norm(v)
+        if norm == 0:
+            return v
+        return v / norm
+
     def __get_zero_slice(self, image_file):
         """ Gets the first slice, transforms the array and returns it. """
         img = nib.load(image_file)
         img_data = img.get_data()
         slice = img_data[:, :, 0].T
-        transformed = transform.resize(slice, (572, 572))
-        return torch.from_numpy(transformed).float()
+        transformed = transform.resize(slice, (512, 512))
+        return self._normalize(transformed)
