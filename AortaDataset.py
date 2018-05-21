75ca99b8e8c2b852914471bb027f516a19f7c66f
Joshy Cyriac <j.cyriac@gmail.com>
2018-05-21 20:00:11 +0200

    Some training and inference, seemt not to work. Need to find out why...

--- AortaDataset.py
+++ AortaDataset.py
@@ -10,37 +10,36 @@ import numpy as np
 class AortaDataset(Dataset):
     """ Aorta Dataset helper class for pytorch. """
 
-    def __init__(self, root_dir='./data'):
-        aorta = './data/**/**/aorta*'
-        cine = './data/**/**/4Ch*'
-        pairs = list(zip(glob.glob(aorta), glob.glob(cine)))
-        self.pairs = pairs
+    def __init__(self, subdir='train'):
+        aorta = './data/' + subdir + '/**/**/aorta*'
+        cine = './data/' + subdir + '/**/**/4Ch*'
+        self.pairs = list(zip(glob.glob(aorta), glob.glob(cine)))
 
-        
+
     def __len__(self):
         return len(self.pairs)
 
-    
+
     def __getitem__(self, idx):
         mask, image = self.pairs[idx]
         return self._get_zero_slice(image, 572), self._groundtruth(mask, 388)
 
-    
+
     def _normalize(self, v):
         norm = np.linalg.norm(v)
         if norm == 0:
             return v
         return v / norm
 
-    
+
     def _groundtruth(self, mask, size):
         mask = self._get_zero_slice(mask, 388)
         foreground = np.where(mask > 0, 1, 0)
         background = np.where(foreground == 0, 1, 0)
         combined = np.stack((foreground, background))
         return combined
-        
-    
+
+
     def _get_zero_slice(self, image_file, size):
         """ Gets the first slice, transforms the array and returns it. """
         img = nib.load(image_file)
